b5c2d850c2ce809e076360bcc61c0fb32138c116
mmayer <mmayer@dundee.ac.uk>
2020-02-25 08:48:10 +0000

    Create classes

---
+++ src/miklos/mayer/ShopItem.java
@@ -0,0 +1,36 @@
+package miklos.mayer;
+
+public class ShopItem {
+
+    private final String id;
+
+    private String name;
+
+    private float cost;
+
+    public ShopItem(String id, String name, float cost) {
+        this.id = id;
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+}
---
+++ src/miklos/mayer/TreeNode.java
@@ -0,0 +1,18 @@
+package miklos.mayer;
+
+public class TreeNode <E> {
+
+    private E data;
+
+    public TreeNode(E data) {
+        this.data = data;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+}
