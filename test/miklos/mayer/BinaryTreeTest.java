6b286f58a727ce68cb526030c4611f3c3f153260
MiklosMayer <mmayer@dundee.ac.uk>
2020-02-28 14:17:22 +0000

    Add empty tree traversal test

--- test/miklos/mayer/BinaryTreeTest.java
+++ test/miklos/mayer/BinaryTreeTest.java
@@ -19,6 +19,12 @@ class BinaryTreeTest {
 
     @Test
     @Order(1)
+    void traverseEmptyList_inOrder() {
+        assertEquals(new ArrayList<>(), tree.traverseInOrder());
+    }
+
+    @Test
+    @Order(2)
     void addOneItem() {
         try {
             tree.add(5);
@@ -29,7 +35,7 @@ class BinaryTreeTest {
     }
 
     @Test
-    @Order(2)
+    @Order(3)
     void addFiveMoreItem_TraverseInOrder() {
         try {
             tree.add(1);
@@ -40,17 +46,17 @@ class BinaryTreeTest {
         } catch (DuplicateItemException e) {
             fail("There shouldn't be any exception");
         }
-        assertEquals(tree.traverseInOrder(), new ArrayList<>(List.of(1, 4, 5, 6, 8, 10)));
+        assertEquals(new ArrayList<>(List.of(1, 4, 5, 6, 8, 10)), tree.traverseInOrder());
     }
 
     @Test
-    @Order(3)
+    @Order(4)
     void addDuplicateItem() {
         assertThrows(DuplicateItemException.class, () -> tree.add(8));
     }
 
     @Test
-    @Order(4)
+    @Order(5)
     void testContainsMethod() {
         assertTrue(tree.contains(1));
         assertTrue(tree.contains(4));
