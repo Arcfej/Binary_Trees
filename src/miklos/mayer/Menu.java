5b60be030a52016ddb4fcd423a715dc01fbf6857
Noki 2 <mayer.miklos91@gmail.com>
2020-03-02 10:44:40 +0000

    Add comments

--- src/miklos/mayer/Menu.java
+++ src/miklos/mayer/Menu.java
@@ -5,9 +5,19 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.function.Predicate;
 
+/**
+ * Represents a Menu for the shop-catalogue program.
+ */
 public class Menu {
 
+    /**
+     * Content separator line for displaying purposes
+     */
     public static final String LINE_SEPARATOR = "-------------------------------------";
+
+    /**
+     * A list of eligible menu options for displaying purposes
+     */
     public static final String MENU = "1) Add a new product to your shop\n" +
             "2) Print shop catalogue\n" +
             "3) Find product\n" +
@@ -17,23 +27,38 @@ public class Menu {
             "7) Replace stock\n" +
             "0) Exit\n";
 
+    /**
+     * The shop of the program.
+     */
     private Shop shop;
 
+    /**
+     * Base constructor
+     */
     public Menu() {
         shop = new Shop();
         restoreCatalogue();
     }
 
+    /**
+     * The entry point of the program. Displays the menu to the user in the first place.
+     *
+     * @param args the starting algorithms of the program.
+     */
     public static void main(String[] args) {
         Menu menu = new Menu();
         menu.displayMenu();
     }
 
+    /**
+     * Displays the menu to the user and execute methods based on their choice.
+     * Save the catalogue after every modifications made to it.
+     */
     public void displayMenu() {
         Scanner in = new Scanner(System.in);
         while (true) {
             System.out.println(MENU);
-
+            // Get menu chose from the user
             switch (in.nextLine()) {
                 case "0":
                     System.out.println("Goodbye!");
@@ -45,7 +70,7 @@ public class Menu {
                     break;
                 case "2":
                     List<Product> productList = shop.getOrderedCatalogue();
-                    printCatalogue(productList);
+                    printProducts(productList);
                     break;
                 case "3":
                     findProduct(in);
@@ -55,12 +80,15 @@ public class Menu {
                     break;
                 case "5":
                     deleteProduct(in);
+                    saveCatalogue();
                     break;
                 case "6":
                     sellProduct(in);
+                    saveCatalogue();
                     break;
                 case "7":
                     increaseStock(in);
+                    saveCatalogue();
                     break;
                 default:
                     System.out.println("Not a valid command!");
@@ -69,6 +97,12 @@ public class Menu {
         }
     }
 
+    /**
+     * Takes input from the user for a new product, validates it and add it to the shop.
+     *
+     * @param in The Scanner through the user communicates with the program
+     *
+     */
     private void addProduct(Scanner in) {
         int id = getIntInput(in,
                 "Please enter an ID for the new product:",
@@ -93,11 +127,17 @@ public class Menu {
         try {
             shop.addNewProduct(id, name, cost, quantity);
         } catch (IllegalArgumentException e) {
+            // There shouldn't be any error after the validation
             System.out.println(e.getMessage());
         }
     }
 
-    private void printCatalogue(List<Product> productList) {
+    /**
+     * Prints a list of products to the screen.
+     *
+     * @param productList The list of products to print out.
+     */
+    private void printProducts(List<Product> productList) {
         System.out.printf("|%10s|%20s|%10s|%10s|\n", "ID", "Name", "Cost", "Stock");
         System.out.println(String.format("|%10s|%20s|%10s|%10s|", "", "", "", "").replace(" ", "-"));
         productList.forEach(product ->
@@ -107,6 +147,11 @@ public class Menu {
         System.out.println();
     }
 
+    /**
+     * Gets input from the user for a Product ID, search for it in the catalogue and print it to the user.
+     *
+     * @param in The Scanner through the user communicates with the program
+     */
     private void findProduct(Scanner in) {
         int id = getIntInput(in,
                 "Please enter the ID of the product you're looking for:",
@@ -117,14 +162,22 @@ public class Menu {
         if (product == null) {
             System.out.println("The required product is not in the shop");
         } else {
-            printCatalogue(List.of(product));
+            printProducts(List.of(product));
         }
     }
 
+    /**
+     * Prints the total cost of all the Products in the catalogue.
+     */
     private void printTotalCost() {
         System.out.println("The total cost of products is £" + shop.getTotalCost());
     }
 
+    /**
+     * Gets input from the user for Product ID and delete it from the shop catalogue.
+     *
+     * @param in The Scanner through the user communicates with the program
+     */
     private void deleteProduct(Scanner in) {
         int id = getIntInput(in,
                 "Please enter the ID of the product you'd like to delete:",
@@ -138,6 +191,12 @@ public class Menu {
         }
     }
 
+    /**
+     * Gets input from the user for a Product ID and a quantity of how much they has sold of it,
+     * then decrease the stock level in the catalogue.
+     *
+     * @param in The Scanner through the user communicates with the program
+     */
     private void sellProduct(Scanner in) {
         int id;
         Product product;
@@ -162,7 +221,12 @@ public class Menu {
         );
         product.decreaseStock(quantity);
     }
-
+    /**
+     * Gets input from the user for a Product ID and a quantity of how much they would like to increase the stock,
+     * then increase the product's stock.
+     *
+     * @param in The Scanner through the user communicates with the program
+     */
     private void increaseStock(Scanner in) {
         int id;
         Product product;
@@ -179,7 +243,6 @@ public class Menu {
                 break;
             }
         }
-        final Product finalProduct = product;
         int quantity = getIntInput(in,
                 "Please enter the additional quantity of the product:",
                 "Please enter a whole number bigger than 0",
@@ -188,6 +251,15 @@ public class Menu {
         product.increaseStock(quantity);
     }
 
+    /**
+     * Gets a validated String input from the user. Repeats the asking until the user provides a valid input.
+     *
+     * @param in The Scanner through the user communicates with the program
+     * @param question The text to display to the user before getting the input
+     * @param errorMessage The message to display to the user after a failed validation
+     * @param validation the condition for validation
+     * @return The validated input from the user.
+     */
     private String getTextInput(Scanner in, String question, String errorMessage, Predicate<String> validation) {
         String input;
         while (true) {
@@ -202,6 +274,15 @@ public class Menu {
         return input;
     }
 
+    /**
+     * Gets a validated float input from the user. Repeats the asking until the user provides a valid input.
+     *
+     * @param in The Scanner through the user communicates with the program
+     * @param question The text to display to the user before getting the input
+     * @param errorMessage The message to display to the user after a failed validation
+     * @param validation the condition for validation
+     * @return The validated input from the user.
+     */
     private float getFloatInput(Scanner in, String question, String errorMessage, Predicate<Float> validation) {
         float input;
         while (true) {
@@ -221,6 +302,15 @@ public class Menu {
         return input;
     }
 
+    /**
+     * Gets a validated integer input from the user. Repeats the asking until the user provides a valid input.
+     *
+     * @param in The Scanner through the user communicates with the program
+     * @param question The text to display to the user before getting the input
+     * @param errorMessage The message to display to the user after a failed validation
+     * @param validation the condition for validation
+     * @return The validated input from the user.
+     */
     private int getIntInput(Scanner in, String question, String errorMessage, Predicate<Integer> validation) {
         int input;
         while (true) {
@@ -240,10 +330,18 @@ public class Menu {
         return input;
     }
 
+    /**
+     * Getter of the shop object.
+     *
+     * @return the shop object.
+     */
     public Shop getShop() {
         return shop;
     }
 
+    /**
+     * Save the state of the catalogue into a txt file.
+     */
     private void saveCatalogue() {
         try (PrintWriter writer = new PrintWriter(new FileWriter("shop.txt"))) {
             for (Product product : shop.getCatalogueForSave()) {
@@ -254,6 +352,9 @@ public class Menu {
         }
     }
 
+    /**
+     * Restore the shop catalogue from the previously saved txt file.
+     */
     private void restoreCatalogue() {
         try (Scanner scanner = new Scanner(new File("shop.txt"))) {
             while (scanner.hasNextLine()) {
@@ -266,6 +367,9 @@ public class Menu {
         }
     }
 
+    /**
+     * Reset the shop, delete all the products in it.
+     */
     public void resetShop() {
         shop = new Shop();
     }
--- src/miklos/mayer/Product.java
+++ src/miklos/mayer/Product.java
@@ -1,15 +1,38 @@
 package miklos.mayer;
 
+/**
+ * Represents a product in the shop
+ */
 public class Product {
 
+    /**
+     * The ID of the product.
+     */
     private final int id;
 
+    /**
+     * The name of the product.
+     */
     private String name;
 
+    /**
+     * The cost of the product.
+     */
     private float cost;
 
+    /**
+     * The stock level of the product.
+     */
     private int stock;
 
+    /**
+     * Base constructor.
+     *
+     * @param id The ID of the product
+     * @param name The name of the product
+     * @param cost The cost of the product
+     * @param quantity The initial stock level of the product
+     */
     public Product(int id, String name, float cost, int quantity) {
         this.id = id;
         this.name = name;
@@ -17,26 +40,57 @@ public class Product {
         this.stock = quantity;
     }
 
+    /**
+     * Getter of the product's ID.
+     *
+     * @return the ID of the product.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Getter of the name of the product.
+     *
+     * @return the name of the product.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Setter of the name of the product.
+     *
+     * @param name the new name of the product.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Getter of the cost of the product.
+     *
+     * @return the cost of the product.
+     */
     public float getCost() {
         return cost;
     }
 
+    /**
+     * Setter of the cost of the product.
+     *
+     * @param cost the new cost of the product.
+     */
     public void setCost(float cost) {
         this.cost = cost;
     }
 
+    /**
+     * Increase the stock level of the product.
+     *
+     * @param quantity The quantity to increase the stock level with.
+     * @return
+     */
     public int increaseStock(int quantity) {
         this.stock += quantity;
         return this.stock;
