84e2a9229af6cbe7d3f42a461da962f2cbd8bc23
mmayer <mmayer@dundee.ac.uk>
2020-03-02 13:51:26 +0000

    Add more comments

--- src/miklos/mayer/DuplicateItemException.java
+++ src/miklos/mayer/DuplicateItemException.java
@@ -1,5 +1,8 @@
 package miklos.mayer;
 
+/**
+ * Thrown when a new data attempted to add to the binary tree with an already existing key.
+ */
 public class DuplicateItemException extends Exception {
 
     public DuplicateItemException(String message) {
